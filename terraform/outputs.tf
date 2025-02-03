375e9424307515f6104ffb504c8421b36dbc4cf8
4renwald <arnaud@arenwald.com>
2025-02-02 22:27:11 -0500

    Added container ressources

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,3 +6,7 @@ output "admin_password" {
   sensitive = true
   value     = azurerm_windows_virtual_machine.main.admin_password
 }
+
+output "container_ipv4_address" {
+  value = azurerm_container_group.container.ip_address
+}
